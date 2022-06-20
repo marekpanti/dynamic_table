aab755bf43fd3fe21dc3ab8abe95db90e1c2e840
Marek Panti <marek.panti@dashly.com>
2022-06-20 23:23:35 +0200

    feat: build

--- src/app/table/table.module.ts
+++ src/app/table/table.module.ts
@@ -6,12 +6,9 @@ import { TableComponent } from './components/table/table.component';
 
 import { TableRoutingModule } from './table-routing.module';
 
-import { MatTableModule } from '@angular/material/table';
-import { MatButtonModule } from '@angular/material/button';
 import { CommonModule } from '@angular/common';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import {MatInputModule} from '@angular/material/input';
 import { SummaryComponent } from './components/summary/summary.component';
+import { MaterialModule } from '../material.module';
 
 @NgModule({
   declarations: [TableComponent, SummaryComponent],
@@ -20,10 +17,7 @@ import { SummaryComponent } from './components/summary/summary.component';
     FormsModule,
     ReactiveFormsModule,
     TableRoutingModule,
-    MatTableModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatButtonModule
+    MaterialModule
   ],
   providers: [],
 })

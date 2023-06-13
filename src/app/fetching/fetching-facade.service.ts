15698b407fc506b88e7342f8498ac1ecdf542ae5
marekpanti@gmail.com <marek@injurymap.com>
2023-06-13 13:08:28 +0200

    feat: angular signals after fetching the data

--- src/app/fetching/fetching-facade.service.ts
+++ src/app/fetching/fetching-facade.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import { Injectable, signal } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { tap } from 'rxjs/operators';
 import { FetchingApiService } from './fetching-api.service';
@@ -7,6 +7,7 @@ import { ToDoInterface } from './models/todo.interface';
 @Injectable()
 export class FetchingFacadeService {
   public allToDos$ = new BehaviorSubject(null);
+  public allTodosSignal = signal([]);
 
   constructor(private fetchService: FetchingApiService) {}
 
@@ -14,6 +15,7 @@ export class FetchingFacadeService {
     return this.fetchService.fetchTodos().pipe(
       tap((todos: ToDoInterface[]) => {
         this.allToDos$.next(todos);
+        this.allTodosSignal.set(todos);
       })
     );
   }
